a2d0654bcc1f900055832db982e0a83d56d619b9
Oleksandr Boldyshev <alex.boldyshev@phloema.com>
2024-04-14 15:52:27 +0200

    Test commit

---
+++ ServerLiveChat/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using ServerLiveChat.Models;
+
+namespace ServerLiveChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        public async Task JoinChat(User user)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "admin", $"{user.Name} has joined");
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ ServerLiveChat/Models/UserModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ServerLiveChat.Models
+{
+    public class User
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
