0a8a4259e9c6ed085739a30116e887af9cd94d76
Oleksandr Boldyshev <alex.boldyshev@phloema.com>
2024-04-14 17:03:22 +0200

    Add data per ogni messaggioConfig Cors

--- ServerLiveChat/Hubs/ChatHub.cs
+++ ServerLiveChat/Hubs/ChatHub.cs
@@ -8,12 +8,14 @@ namespace ServerLiveChat.Hubs
 
         public async Task JoinChat(User user)
         {
-            await Clients.All.SendAsync("ReceiveMessage", "admin", $"{user.Name} has joined");
+            await Clients.All.SendAsync("ReceiveMessage", $"{user.Name} has joined");
         }
 
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string message, string timestamp)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", user, message, timestamp);
         }
+
+
     }
 }
