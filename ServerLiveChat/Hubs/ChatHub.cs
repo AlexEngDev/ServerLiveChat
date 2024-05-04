c7ab40da49e68dba1c7707780c37d6d520ab8a22
Oleksandr Boldyshev <alex.boldyshev@phloema.com>
2024-05-04 16:45:33 +0200

    Fix bug

--- ServerLiveChat/Hubs/ChatHub.cs
+++ ServerLiveChat/Hubs/ChatHub.cs
@@ -11,10 +11,16 @@ namespace ServerLiveChat.Hubs
             await Clients.All.SendAsync("UserJoin", user);
         }
 
-        public async Task SendMessage(string user, string message, string timestamp)
+        public async Task SendMessage(string user, string message, string timestamp, string latitude, string longitude)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message, timestamp);
+            DateTime dateTime = DateTime.Parse(timestamp);
+            string formattedTimestamp = dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+
+            Console.WriteLine($"User: {user} Message: {message} Timestamp: {formattedTimestamp} Latitude: {latitude} Longitude: {longitude}");
+
+            await Clients.All.SendAsync("ReceiveMessage", user, message, formattedTimestamp);
         }
+
 
 
     }
